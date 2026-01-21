47268c838728588700a984996f59901b12044b48
Robert Stosick <rstosick@gmail.com>
2026-01-21 15:02:57 -0800

    Added 547_Number_of_Provinces.java

---
+++ 547_Number_of_Provinces.java
@@ -0,0 +1,70 @@
+class UnionFind {
+
+    private int[] root;
+    private int[] rank;
+    private int numRoots;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for(int i=0; i<size;i++) {
+            root[i] = i;
+            rank[i] = 1;
+        } 
+        // Arrays.fill(rank, 1); 
+        numRoots = size;
+    }
+
+    public int find(int x) {
+        if(x==root[x]) return x;
+        else return root[x]=find(root[x]);
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX!=rootY) {
+            if(rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if(rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] = rank[rootX] + 1;
+            }
+            numRoots--;
+        }
+        
+    }
+    public boolean connected(int x, int y) {
+        return find(x)==find(y);
+    }
+
+    public int countRoots() {
+        return numRoots;
+    }
+
+    // public int countRoots() {
+    //     int count = 0;
+    //     for(int i=0; i<root.length; i++) {
+    //         if(root[i]==i) count++;
+    //     }
+    //     return count;
+    // }
+}
+
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        // The array is specified as dimensions NxN
+        int n = isConnected[0].length;
+        UnionFind uf = new UnionFind(n);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(isConnected[i][j]==1) {
+                    uf.union(i, j);
+                }    
+            }
+        }
+        return uf.countRoots();
+    }
+}
