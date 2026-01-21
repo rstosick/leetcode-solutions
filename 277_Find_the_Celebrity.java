b0d78dffb8143a2494c888b4f8a0d4d11ff1abda
Robert Stosick <rstosick@gmail.com>
2026-01-21 15:06:19 -0800

    Added 277_Find_the_Celebrity.java

---
+++ 277_Find_the_Celebrity.java
@@ -0,0 +1,31 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int j;
+        boolean isCelebrity;
+        for(int i=0; i<n; i++) {
+            // Start by assuming i is indeed the celebrity
+            isCelebrity = true;
+            for(j=0; j<n; j++) {
+                if(i==j) {
+                    continue;
+                };
+                if(knows(j,i)==false) {
+                    // Case: someone doesn't know them
+                    isCelebrity = false;
+                    break;
+                } else {
+                    if(knows(i,j)==true) {
+                        // Case: they know someone else
+                        isCelebrity=false;
+                        break;
+                    }
+                }
+            }
+            if(isCelebrity==true) return i;
+        }
+        return -1;
+    }
+}
