e92720697de46edbbea5464aa5cd3b2d3b8a98c1
Robert Stosick <rstosick@gmail.com>
2026-01-21 14:39:17 -0800

    Added 11_Container_With_Most_Water.java

---
+++ 11_Container_With_Most_Water.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int maxArea(int[] height) {
+        if (height.length < 2)
+            throw new IllegalArgumentException("input is too small");
+        int left = 0;
+        int right = height.length - 1;
+        int currentArea;
+        int biggestArea = 0;
+
+        while (left < right) {
+            currentArea = (right - left) * Math.min(height[left], height[right]);
+            if (currentArea > biggestArea) {
+                biggestArea = currentArea;
+            }
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return biggestArea;
+    }
+}
