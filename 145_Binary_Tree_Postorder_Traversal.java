b99ad18c6e900bc8b9b4693e26545b2c00ca4c4c
Robert Stosick <rstosick@gmail.com>
2026-01-21 16:13:37 -0800

    Add 145_Binary_Tree_Postorder_Traversal.java

---
+++ 145_Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// Post-order Traversal
+// Post-order traversal is to traverse the left subtree first.
+// Then traverse the right subtree. Finally, visit the root.
+
+class Solution {
+
+    private List<Integer> result = null;
+
+    private void helper(TreeNode current) {
+        if(current==null) return;
+        helper(current.left);
+        helper(current.right);
+        result.add(current.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        result = new ArrayList<>();
+        helper(root);
+        return result;
+    }
+}
