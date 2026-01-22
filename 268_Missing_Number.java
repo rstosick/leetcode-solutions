858adb25a34965742ba28cdaf640da85271bb3d4
Robert Stosick <rstosick@gmail.com>
2026-01-21 16:18:24 -0800

    Add 268_Missing_Number.java

---
+++ 268_Missing_Number.java
@@ -0,0 +1,14 @@
+// Hasty, inefficient solution
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int max = nums[len-1];
+        for(int i=0;i<len;i++)
+        {
+            if(nums[i]!=i) return i;
+        }
+        return nums.length;
+    }
+}
