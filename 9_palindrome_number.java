e9414ef752a3faa93ed46cd442a0047e6aa206bf
Robert Stosick <rstosick@gmail.com>
2026-01-21 17:08:39 -0800

    Revert "Fix case"
    This reverts commit b209b7c002171bc35fd79c734a3ca8cc9a349fc4.

--- 9_palindrome_number.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-  public boolean isPalindrome(int x) {
-    String input = String.valueOf(x);
-    for (int i = 0; i < input.length() / 2; i++) {
-      if (input.charAt(i) != input.charAt(input.length() - i - 1)) return false;
-    }
-    return true;
-  }
-}
