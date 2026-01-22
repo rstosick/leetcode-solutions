da3fcd0a9bf80bf64aaaa8cc91c13061a3f0a571
Robert Stosick <rstosick@gmail.com>
2026-01-21 16:17:48 -0800

    Add 242_Valid_Anagram.java

---
+++ 242_Valid_Anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map1 = new HashMap<>(26*2);
+        Map<Character, Integer> map2 = new HashMap<>(26*2);
+        for(char c: s.toCharArray()) {
+            map1.merge(c, 1, Integer::sum);
+        }
+        for(char c: t.toCharArray()) {
+            map2.merge(c, 1, Integer::sum);
+        }
+        return map1.equals(map2);
+    }
+}
